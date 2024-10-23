bf4ff70c80af6f6c577669832d195906f40c48de
MANASA D <manasad.xworkz@gmail.com>
2024-10-23 09:18:21 +0530

    collection

---
+++ Collection-size, add/eyeCareArrayListString.java
@@ -0,0 +1,21 @@
+package com.xworkz.eyeCare.runner;
+
+import java.util.ArrayList;
+
+
+public class eyeCareArrayListString {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> set = new ArrayList<String>();
+
+		set.add("Bheema");
+		set.add("Arjuna");
+		set.add("Balarama");
+		set.add("Nakula");
+		set.add("Bargav");
+
+		System.out.println(set.size());
+	}
+
+}
---
+++ Collection-size, add/eyeCareLinkedListString.java
@@ -0,0 +1,22 @@
+package com.xworkz.eyeCare.runner;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.SortedSet;
+
+public class eyeCareLinkedListString {
+	
+	public static void main(String[] args) {
+		
+		LinkedList<String> set =   new LinkedList<String>();
+		
+		set.add("Bheema");
+		set.add("Arjuna");
+		set.add("Balarama");
+		set.add("Nakula");
+		
+		System.out.println(set.size());
+	}
+
+}
