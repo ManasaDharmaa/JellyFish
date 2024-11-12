d7fa2fb58e0719eacb8ce90151943dccce62bdfc
MANASA D <manasad.xworkz@gmail.com>
2024-11-12 21:24:11 +0530

    Enum

---
+++ Enum/EnumContainer.java
@@ -0,0 +1,15 @@
+package com.xworkz.enumcontainer.container;
+
+public enum EnumContainer {
+	
+	Milk("100"), Butter("250"), Cream("560"), IceCream("370");
+
+	private EnumContainer(String value) {
+		
+		System.out.println(value);
+	}
+	
+	
+	
+
+}
---
+++ Enum/EnumRunner.java
@@ -0,0 +1,14 @@
+package com.xworkz.enumcontainer.runner;
+
+import com.xworkz.enumcontainer.container.EnumContainer;
+
+public class EnumRunner {
+	
+	public static void main(String[] args) {
+		
+		EnumContainer butter = EnumContainer.Milk;
+		
+		System.err.println(butter);
+	}
+
+}
