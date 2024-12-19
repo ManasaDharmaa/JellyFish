d6333bdbfe4841420eee897860fb7f3cfbc6db95
MANASA D <manasad.xworkz@gmail.com>
2024-12-19 17:39:44 +0530

    JDBC steps

---
+++ JDBC-steps/country/src/main/java/com/xworkz/country/runner/USARunner.java
@@ -0,0 +1,24 @@
+package com.xworkz.country.runner;
+
+import java.sql.*;
+
+public class USARunner {
+
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("file found");
+
+            String url = "jdbc:mysql://localhost:3306/ott";
+            String userName = "root";
+            String password = "root";
+            DriverManager.getConnection(url, userName, password);
+
+            System.out.println("connection successfully");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
---
+++ JDBC-steps/onlineShopping/src/main/java/com/xworkz/onlineShopping/runner/Flipkart.java
@@ -0,0 +1,24 @@
+package com.xworkz.onlineShopping.runner;
+
+import java.sql.*;
+
+public class Flipkart {
+
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("file found");
+
+            String url = "jdbc:mysql://localhost:3306/onlineShopping";
+            String userName = "root";
+            String password = "root";
+            DriverManager.getConnection(url, userName, password);
+
+            System.out.println("connection successfully");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
